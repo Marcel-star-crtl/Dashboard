288c61b479ebe82182b4b81020a58f8fe7b8b8be
Marcel-star-crtl <marcelngong50@gmail.com>
2024-10-26 06:16:18 +0100

    more on routes

--- src/App.js
+++ src/App.js
@@ -15,18 +15,43 @@
 
 
 
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import routes, { renderRoutes } from './routes';
+
+// const App = () => {
+//   // Only use basename if it's defined
+//   const basename = process.env.REACT_APP_BASE_NAME || '/';
+  
+//   return (
+//     <BrowserRouter basename={basename}>
+//       {renderRoutes(routes)}
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+
+
+
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import routes, { renderRoutes } from './routes';
 
 const App = () => {
-  // Only use basename if it's defined
+  // Get basename from environment or default to '/'
   const basename = process.env.REACT_APP_BASE_NAME || '/';
   
+  // Get public URL from environment
+  const publicUrl = process.env.PUBLIC_URL || '';
+  
   return (
-    <BrowserRouter basename={basename}>
-      {renderRoutes(routes)}
-    </BrowserRouter>
+    <React.Fragment>
+      <BrowserRouter basename={basename}>
+        {renderRoutes(routes)}
+      </BrowserRouter>
+    </React.Fragment>
   );
 };
 
